b7bb3630f23b6f272f41a65b075302997675d937
unknown <alejandro.quesada@gmail.com>
2025-04-15 23:10:09 -0500

    Validaciones previas

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
-def calculate() -> float:
-    pass
+def calculate(e):
+
+    if not e.strip():
+        raise ValueError("La expresión está vacía o contiene solo espacios.")
+
+    valid_chars = "0123456789.+-*/() "
+    if any(char not in valid_chars for char in e):
+        raise ValueError("La expresión contiene caracteres no válidos.")
